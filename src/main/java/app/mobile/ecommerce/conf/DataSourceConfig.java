7e37835877beab16bcdeeb08dfb04b9db9aa9bc9
SouzaGustavoGG <souzagustavogg@gmail.com>
2019-06-21 17:00:44 -0300

    liquibase

--- src/main/java/app/mobile/ecommerce/conf/DataSourceConfig.java
+++ src/main/java/app/mobile/ecommerce/conf/DataSourceConfig.java
@@ -6,6 +6,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
 
+import liquibase.integration.spring.SpringLiquibase;
+
 @Configuration
 public class DataSourceConfig {
      
@@ -17,5 +19,13 @@ public class DataSourceConfig {
 		    dataSource.setUsername("spring.datasource.username");
 		    dataSource.setPassword("spring.datasource.password");
 		    return dataSource;
+    }
+    
+    @Bean
+    public SpringLiquibase liquibase() {
+        SpringLiquibase liquibase = new SpringLiquibase();
+        liquibase.setChangeLog("classpath:liquibase-changeLog.xml");
+        liquibase.setDataSource(getDataSource());
+        return liquibase;
     }
 }
